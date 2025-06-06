1bc7bbd34ba4447dae4788b2de238f2059d3354b
nishizhen <nishizhen@gmail.com>
2025-06-06 16:41:01 +0800

    allow all domain

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,9 @@ from routes import router
 
 app = FastAPI(title="AnticlockwiseSearch Backend", version="0.1.0")
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # 允许所有域
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
